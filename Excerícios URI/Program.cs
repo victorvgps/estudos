087c3fceda6909d76932c15c8d4de0f8a706db2d
Victor <victorvgps@hotmail.com>
2021-05-06 01:12:01 -0300

    Atualização

--- Excerícios URI/Program.cs
+++ Excerícios URI/Program.cs
@@ -7,17 +7,18 @@ namespace Excerícios_URI
     {
         static void Main(string[] args)
         {
-            int peca1, peca2, quant1, quant2;
+            int quant1, quant2;
+            string peca1, peca2;
             double val1, val2, total;
 
-            Console.WriteLine("Informe o código, a quantidade e o valor do primeiro produto.");
+            Console.WriteLine("Informe o nome, a quantidade e o valor do primeiro produto.");
             string[] valores = Console.ReadLine().Split(' ');
-            peca1 = int.Parse(valores[0]);
+            peca1 = (valores[0]);
             quant1 = int.Parse(valores[1]);
             val1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
-            Console.WriteLine("Informe o código, a quantidade e o valor do segundo produto.");
+            Console.WriteLine("Informe o nome, a quantidade e o valor do segundo produto.");
             valores = Console.ReadLine().Split(' ');
-            peca2 = int.Parse(valores[0]);
+            peca2 = (valores[0]);
             quant2 = int.Parse(valores[1]);
             val2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
