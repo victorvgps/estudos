44476f3b39a3506d4de30db30bc6f45adb23d8e0
Victor <victorvgps@hotmail.com>
2021-05-07 01:38:25 -0300

    Primeiro Exercício de Estrutura Condicionada

--- Exercício/Program.cs
+++ Exercício/Program.cs
@@ -7,40 +7,21 @@ namespace Exercício
     {
         static void Main(string[] args)
         {
-            int W1, X1, Y1, Z1, W2, X2, Y2, Z2, W, X, Y, Z, inicio, fim, duracao, resto;
+            double nota1, nota2, notaFinal;
 
-            string[] valores = Console.ReadLine().Split(' ');
-            W1 = int.Parse(valores[1]);
-
-            valores = Console.ReadLine().Split(' ');
-            X1 = int.Parse(valores[0]);
-            Y1 = int.Parse(valores[2]);
-            Z1 = int.Parse(valores[4]);
-
-            valores = Console.ReadLine().Split(' ');
-            W2 = int.Parse(valores[1]);
-
-            valores = Console.ReadLine().Split(' ');
-            X2 = int.Parse(valores[0]);
-            Y2 = int.Parse(valores[2]);
-            Z2 = int.Parse(valores[4]);
-
-            inicio = (W1 - 1) * 24 * 60 * 60 + X1 * 60 * 60 + Y1 * 60 + Z1;
-            fim = (W2 - 1) * 24 * 60 * 60 + X2 * 60 * 60 + Y2 * 60 + Z2;
-
-            duracao = fim - inicio;
-
-            W = duracao / (24 * 60 * 60);
-            resto = duracao % (24 * 60 * 60);
-            X = resto / (60 * 60);
-            resto = resto % (60 * 60);
-            Y = resto / 60;
-            Z = resto % 60;
-
-            Console.WriteLine(W + " dia(s)");
-            Console.WriteLine(X + " hora(s)");
-            Console.WriteLine(Y + " minuto(s)");
-            Console.WriteLine(Z + " segundo(s)");
+            Console.WriteLine("Insira a nota das duas provas");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            notaFinal = nota1 + nota2;
+            
+            if (notaFinal < 60) {
+                Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+            }
+            else {
+                Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("APROVADO");
+                }
+            }
         }
     }
-}
