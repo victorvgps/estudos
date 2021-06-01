055938e7a63b0c92b3d8cb7d358dbbdd5004692d
Victor <victorvgps@hotmail.com>
2021-05-31 21:03:48 -0300

    Estutura Repetitiva

--- Exercício/Program.cs
+++ Exercício/Program.cs
@@ -7,70 +7,15 @@ namespace Exercício
     {
         static void Main(string[] args)
         {
-            string palavra1, palavra2, palavra3;
-            palavra1 = Console.ReadLine();
-            palavra2 = Console.ReadLine();
-            palavra3 = Console.ReadLine();
 
-            if (palavra1 == "vertebrado")
-            {
-                if (palavra2 == "mamifero")
-                {
-                    if (palavra3 == "onivoro")
-                    {
-                        Console.WriteLine("homem");
-                    }
-                    else
-                    {
-                        Console.WriteLine("vaca");
-
-                    }
-                }
+            int x, soma;
+                x = int.Parse(Console.ReadLine());
+            soma = 0;
+            while (x != 0) {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
             }
-            {
-                if (palavra2 == "ave")
-                {
-                    if (palavra3 == "onivoro")
-                    {
-                        Console.WriteLine("pomba");
-                    }
-                    else
-                    {
-                        Console.WriteLine("aguia");
-                    }
-                }
-            }
-            if (palavra1 == "invertebrado")
-            {
-                if (palavra2 == "inseto")
-                {
-                    if (palavra3 == "hematofago")
-                    {
-                        Console.WriteLine("pulga");
-                    }
-                    else
-                    {
-                        Console.WriteLine("lagarta");
-                    }
-                }
-            }
-                    if (palavra2 == "anelideo")
-                    {
-                        if (palavra3 == "hematofago")
-                        {
-                            Console.WriteLine("sanguessuga");
-                        }
-                        else
-                        {
-                            Console.WriteLine("minhoca");
-                        }
-                    }
-                }
-            }
-        }           
-
-
-
-
-
-
+            Console.WriteLine(soma);
+        }
+    }
+}
