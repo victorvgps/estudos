3f11ce5f8b086313200d55e2834c0e8bca82f65f
Victor <victorvgps@hotmail.com>
2022-03-16 23:57:29 -0300

    Exercício Composição e Enumeração

---
+++ Enumerações e Composições/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumerações_e_Composições.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {    
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Enumerações e Composições/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumerações_e_Composições.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior =2
+    }
+}
